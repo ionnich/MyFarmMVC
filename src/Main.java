ebde2c23397fbbfa87b42ad1009eb842a3951b66
Aaron Nicholas Gumapac <aarn.gmpc@gmail.com>
2022-11-29 19:22:10 +0800

    init javafx primitive project

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+import javafx.application.Application;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
